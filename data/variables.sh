cf60cb4e43b7b4e8e9104af7b5ffed8ebde167fa
leoyong <y_c_l__@live.com>
2015-07-15 12:08:24 +0800

    build main.sh

---
+++ data/variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#This file is used to hold variables whic collecting data
+
+
+
+#path
+
+data_path="collected_data" ;
+
+export data_path ;
+
+echo "variables is set"
---
+++ main.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+
+# this is the main executable file
+
+#including script to here
+
+source data/Data_gen.sh
+
+data_gen

d37f051c353b0aa1df491a92a342cafdb9dba9d4
leoyong <y_c_l__@live.com>
2015-08-12 09:28:21 +0800

    Add comment to Arduino code

--- arduino_code/sketch_jul17a/sketch_jul17a.ino
+++ arduino_code/sketch_jul17a/sketch_jul17a.ino
@@ -1,49 +1,61 @@
-// this is a simple program to generate data
-// data transmitted via serial port.
-// 
+/* this is a simplpe code to read sensor and send it over serial to the computer
+ 
+ This have time function that allow user to:
+ 
+ 1) Select the time interval for individual data.
+ 2) Set the duration for data collection.
+ 
+ Auth: LEO YONG
+ 
+ LeoYong95/sense
+ 
+*/ 
 
 //-----------List down the variables
-int data [500];
-int index =0;
+int data [500]; //-----Limit data to 500 data at one collection.
+int index =0; 
 int increment;
 int updateinterval;
 int updateendtime;
 int endtime;
-//read sensor (Now just generate Data)
+
+
+//-------------read sensor ( for now just generate Data)
 class Sensor
 {
   
   public:
+//------------List Down variables
   unsigned long lastupdate;
-  unsigned long timer;
   int sensorVal;
   
+//------------Constructor  
   Sensor(int interval, int endtime)
   {
-    //-------------set variables value
+//-------------set variables value
     updateinterval = interval;
     updateendtime =endtime;
     sensorVal =0;
     increment =1;
-    timer = millis();
+    
   }
 
+//--------------call this function for every interval
   void update()
   {
-    //---------------call every 0.1 sec
+
     if((millis() -lastupdate) > updateinterval)
     {
-      //loop of data here
      lastupdate = millis();
       sensorVal += increment;
       index += increment ;
-      data [index] =sensorVal;
+      data [index] =sensorVal; //---------Input data to the array
       Serial.println(sensorVal);
     }
     
   }
   
-  
+//------------call this function for every data collection  
   void kill()
   {
     if ( (millis()- endtime) > updateendtime*1000)
